0c748847379612c729666a8d4198023404584412
mfadhilfajar-oss <m.fadhilfajar@gmail.com>
2025-09-09 16:26:01 +0700

    selesai tugas individu 2

--- main/models.py
+++ main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('futsal shoes', 'Futsal shoes'),
+        ('football shoes', 'Football shoes'),
+        ('socks', 'Socks'),
+    ]
+    
+    name = models.CharField(max_length=255)
+    price = models.IntegerField(default=0)
+    description = models.TextField(default="")
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='football shoes')
+    thumbnail = models.URLField(blank=True, null=True)
+    sold_count = models.PositiveIntegerField(default=0)
+    is_featured = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.name
+    
+    @property
+    def is_bestseller(self):
+        return self.sold_count > 50
+        
+    def increment_sales(self):
+        self.sold_count += 1
+        self.save()
--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+def show_main(request):
+    context = {
+        'name': 'Muhammad Fadhil Al Afifi Fajar',
+        'class': 'PBP C'
+    }
+
+    return render(request, "main.html", context)
+
 # Create your views here.
